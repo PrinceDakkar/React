7a26d1fa7888f0049672fd4728112fd1faeabf38
Nika Gazzaev <princdakkar@gmail.com>
2021-08-04 17:10:51 +0300

    78 lesson

---
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,23 @@
+import Preloader from "../../common/Preloader/Preloader";
+import s from "./ProfileInfo.module.css";
+import ProfileStatus from "./ProfileStatus";
+
+const ProfileInfo = (props) => {
+  if (!props.profile) {
+    return <Preloader />;
+  }
+
+  return (
+    <div>
+      <div className={s.profileImg}>
+        <img src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQYSibIOdUg1VOegwxLDq35TNE8I_FOo-qwyDbeXwS96iUnuWfcyR2ZquLrN2dPqJn1Gza29BUCpYUHGB7wMDQ" />
+      </div>
+      <div className={s.descriptionBlock}>
+        <img src={props.profile.photos.large} />
+        <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
---
+++ my-app/src/components/Users/UsersContainer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { connect } from "react-redux";
+import { follow, setCurrentPage, setUsers, setTotalUsersCount, toggleIsFetching, unfollow, toggleFollowingProgress, getUsers } from '../../redux/users-reducer';
+import * as axios from 'axios';
+import Users from './Users';
+import Preloader from '../common/Preloader/Preloader';
+import { usersAPI } from '../../api/api';
+import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
+
+class UsersContainer extends React.Component{
+
+    componentDidMount(){
+        this.props.getUsers(this.props.currentPage, this.props.pagesSize);
+    }
+
+    onPageChanged = (pageNumber) =>{
+
+        this.props.getUsers(pageNumber, this.props.pagesSize)
+    }
+
+
+    render(){
+        return<>
+        {this.props.isFetching ? <Preloader /> : null}
+        <Users 
+        totalUsersCount={this.props.totalUsersCount}
+        pagesSize={this.props.pagesSize}
+        currentPage={this.props.currentPage}
+        onPageChanged={this.onPageChanged}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+        followingInProgress={this.props.followingInProgress}/>
+        </>
+    }
+}
+
+let mapStateToProps = (state) =>{
+    return {
+        users: state.usersPage.users,
+        pagesSize: state.usersPage.pagesSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
+    }
+}
+
+
+
+
+export default compose(
+    withAuthRedirect,
+    connect(mapStateToProps,{follow,unfollow,setCurrentPage,toggleFollowingProgress,getUsers,}))
+    (UsersContainer)
